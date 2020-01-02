58246caa84e95c2b4ac0e6b12a7bfa2818e29d26
Anton <anton_real_madrid@icloud.com>
2020-01-02 13:43:52 +0100

    adjust header sizing

--- src/components/projectGrid.js
+++ src/components/projectGrid.js
@@ -14,7 +14,7 @@ const ProjectGrid = () => {
           <FadeCard image='https://unsplash.it/800/600?image=83' title='Portfolio' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae.'/>
         </li>
         <li className={projectGridStyles.projectItem}>
-          <FadeCard image='https://unsplash.it/800/600?image=84' title='Discord bot' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae.'/>
+          <FadeCard image='https://unsplash.it/800/600?image=84' title='Discord bot' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. '/>
         </li>
         <li className={projectGridStyles.projectItem}>
           <FadeCard image='https://unsplash.it/800/600?image=85' title='Something else' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, beatae.'/>
