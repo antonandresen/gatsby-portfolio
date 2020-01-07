12a7e12d58ed84cbac46f0762f34f34b1cf3466d
Anton <anton_real_madrid@icloud.com>
2020-01-07 14:28:45 +0100

    removed paragraph project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,12 +10,6 @@ const ProjectsPage = () => {
     <Layout>
       <Head pageTitle="Projects" />
       <h1>Projects</h1>
-      <p>my projects come here.</p>
-      {/* <Card
-        title="Project One"
-        date="2 days ago"
-        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet commodi molestias, a perferendis facilis aspernatur ex ab maiores assumenda est!"
-      />*/}
       <ProjectGrid />
     </Layout>
   );
