eada4e3c846ba1caaec6010ba7e2d3b4dc3e374b
Anton <anton_real_madrid@icloud.com>
2020-01-09 16:12:06 +0100

    added email to contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,7 @@ const ContactPage = () => {
   return (
     <Layout>
       <Head pageTitle="Contact" />
+      <a href="anton_real_madrid@icloud.com">asd</a>
       <h1>Get in touch!</h1>
       <p>Form comes here.</p>
     </Layout>
