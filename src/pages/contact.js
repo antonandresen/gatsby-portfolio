cd895acf5528b68fde3dd823835434bcc1895a02
Anton Andresen <anton_real_madrid@icloud.com>
2020-01-09 18:12:02 +0100

    added contact svg to page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,22 @@ import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
+import contactStyles from './contact.module.scss';
+
 const ContactPage = () => {
   return (
     <Layout>
       <Head pageTitle="Contact" />
-      <a href="anton_real_madrid@icloud.com">asd</a>
-      <h1>Get in touch!</h1>
-      <p>Form comes here.</p>
+      <article className={contactStyles.container}>
+        <section className={contactStyles.illustration}>
+          <img src='/contact.svg'/>
+        </section>
+        <section className={contactStyles.content}>
+          <h1>Get in touch!</h1>
+          <p>Form comes here.</p>
+          <a href="mailto:anton.andresen@hotmail.com?subject=Business opportunity">anton.andresen@hotmail.com</a>
+        </section>
+      </article>
     </Layout>
   );
 };
