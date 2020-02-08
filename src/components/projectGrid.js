26da0a8c693d913d2fee08b4a59e1c34c4c3492c
Anton <anton_real_madrid@icloud.com>
2020-02-08 20:42:54 +0100

    Added .net gymit project to portfolio

--- src/components/projectGrid.js
+++ src/components/projectGrid.js
@@ -13,6 +13,9 @@ const ProjectGrid = () => {
         <li className={projectGridStyles.projectItem}>
           <FadeCard image='https://puu.sh/EVKlA/871e38a044.jpg' title='Gatsby Portfolio' text='A portfolio where i showcase all my software projects. I built the site using React.js and Gatsby' projectLink='https://www.anton.best/' codeLink='https://github.com/Anton1337/gatsby-portfolio' />
         </li>
+        <li className={projectGridStyles.projectItem}>
+          <FadeCard image='https://puu.sh/F8w6B/73e056facc.png' title='GymIT .NET CORE API' text='A REST API build in .NET CORE 3.1 where users can sign up, and keep track of their workouts and different exercises. MySQL, JWT.'projectLink='https://gymitapp.azurewebsites.net/swagger/index.html' codeLink='https://github.com/Anton1337/gymit-server' />
+        </li>
       </ul>
     </section>
   );
